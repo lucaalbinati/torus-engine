179f774892b3a89e8c5071a93a91331d076f6f97
Luca Albinati <luca.albinati@gmail.com>
2021-10-01 14:37:49 +0200

    simplify scene computation

--- scene/plane.py
+++ scene/plane.py
@@ -15,9 +15,26 @@ class Plane:
 		self.pixel_incr_height = pixel_incr_height
 		self.pixel_incr_width = pixel_incr_width
 		self.top_left_corner = self.__compute_top_left_corner()
+		self.init_pixels = self.__compute_init_pixels()
 
 	def __compute_top_left_corner(self):
 		return self.p0 - (self.width / 2) * self.horizontal + (self.height / 2) * self.up
+
+	def __compute_init_pixels(self):
+		pixels = {}
+		for h in range(self.nb_pixel_height):
+			h_incr = - self.pixel_incr_height * h
+			for w in range(self.nb_pixel_width):
+				w_incr = self.pixel_incr_width * w
+
+				point = self.top_left_corner + w_incr * self.horizontal + h_incr * self.up
+
+				i = h * self.nb_pixel_width + w
+				pixels[i] = (point, sys.maxsize, 0)
+		return pixels
+
+	def get_fresh_init_pixels(self):
+		return dict(self.init_pixels)
 
 	def find_intersection(self, vector_start, vector):
 		l0 = vector_start
@@ -33,35 +50,11 @@ class Plane:
 		p = l0 + l * d
 		return p
 
-	def __init_grid(self):
-		grid_points = {}
-
-		for h in range(self.nb_pixel_height):
-			h_incr = - self.pixel_incr_height * h
-			for w in range(self.nb_pixel_width):
-				w_incr = self.pixel_incr_width * w
-
-				point = self.top_left_corner + w_incr * self.horizontal + h_incr * self.up
-
-				i = h * self.nb_pixel_width + w
-				grid_points[i] = (point, sys.maxsize, 0)
-
-		return grid_points
-
-	def clip_points_to_pixels(self, intersections_on_plane):	
-		grid_points = self.__init_grid()
-
-		# find a way to clip each intersection point to the grid (ideally without having to compute [#grid_points * #intersection_points] distances)
-		for (intersection_point, depth, brightness) in intersections_on_plane:
-			tlc_to_intersection_point = intersection_point - self.top_left_corner
-			horizontal_shift = np.dot(self.horizontal, tlc_to_intersection_point)
-			vertical_shift = np.dot(self.up, tlc_to_intersection_point)
-			grid_position_w = int(horizontal_shift / self.pixel_incr_width)
-			grid_position_h = int(- vertical_shift / self.pixel_incr_height)
-			grid_position = grid_position_h * self.nb_pixel_width + grid_position_w
-			if grid_position in grid_points:
-				# replace the point if it is closer to the observer
-				if depth < grid_points[grid_position][1]:
-					grid_points[grid_position] = (intersection_point, depth, brightness)
-
-		return grid_points
+	def clip_point_to_pixel(self, intersection_on_plane):	
+		topleftcorner_to_intersection_point = intersection_on_plane - self.top_left_corner
+		horizontal_shift = np.dot(self.horizontal, topleftcorner_to_intersection_point)
+		vertical_shift = np.dot(self.up, topleftcorner_to_intersection_point)
+		grid_position_w = int(horizontal_shift / self.pixel_incr_width)
+		grid_position_h = int(- vertical_shift / self.pixel_incr_height)
+		grid_position = grid_position_h * self.nb_pixel_width + grid_position_w
+		return grid_position
--- scene/scene.py
+++ scene/scene.py
@@ -15,38 +15,32 @@ class Scene:
 			brightness = self.light_source.compute_brightness_on_point(self.observer, point, normal)
 			self.brightnesses.append(brightness)
 
-	def __compute_intersections_on_plane(self):
-		intersections_on_plane = []
+	def __compute_pixels(self):
+		pixels = self.camera.plane.get_fresh_init_pixels()
 
 		for point, brightness in zip(self.obj.points, self.brightnesses):
 			point_to_observer_vector = np.array(self.observer) - np.array(point)
 			distance_to_point = np.linalg.norm(point_to_observer_vector)
 
-			# verify that the point is in the line of sight of the observer
-			is_visible_to_observer = True
-			step = 0.1
-			for m in np.arange(step, distance_to_point - step, step):
-				point_m = point + m * point_to_observer_vector
-				if self.obj.contains_point(point_m):
-					is_visible_to_observer = False
-					break
-
 			# only project the points on the camera plane that are visible to the observer
-			if is_visible_to_observer and brightness > 0:
+			if brightness > 0:
 				intersection_on_plane = self.camera.plane.find_intersection(self.observer, point_to_observer_vector)
-				intersections_on_plane.append((intersection_on_plane, distance_to_point, brightness))
+				pixel = self.camera.plane.clip_point_to_pixel(intersection_on_plane)
+
+				if not pixel in pixels or distance_to_point < pixels[pixel][1]:
+					pixels[pixel] = (intersection_on_plane, distance_to_point, brightness)
 		
-		return intersections_on_plane
+		return pixels
+
+	def modify_light_source(self, light_source):
+		self.light_source = light_source
 
 	def show(self):
 		self.__compute_illumination()
-		intersections_on_plane = self.__compute_intersections_on_plane()
-
-		pixels_on_plane = self.camera.plane.clip_points_to_pixels(intersections_on_plane)
-
+		pixels = self.__compute_pixels()
 
 		clear_console()
-		for i, (_, _, brightness) in pixels_on_plane.items():
+		for i, (_, _, brightness) in pixels.items():
 			if i % self.camera.plane.nb_pixel_width == 0 and i > 0:
 				print("-")
 			print(get_brightness_char(brightness), end='')
