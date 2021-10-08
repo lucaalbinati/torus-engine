add Rotation animation (TODO: fix normals)

animation.py
@@ -1,28 +1,42 @@
 import time
 import numpy as np
 from utils import normalize_vector
+from scene.camera import Camera
 from scene.light import Light
 
 class Animation:
 
-	def __init__(self, scene, light_source_start_point, light_source_end_point, FPS=5, animation_duration_in_sec=2):
+	# TODO generalize to animating camera and object
+
+	def __init__(self, scene, start_point, end_point, FPS=5, animation_duration_in_sec=2):
 		self.scene = scene
-		self.light_source_start_point = light_source_start_point
+		self.start_point = start_point
 		self.FPS = FPS
 
 		self.nb_frames = FPS * animation_duration_in_sec
-		vector_incr_direction = normalize_vector(light_source_end_point - light_source_start_point)
-		vector_norm = np.linalg.norm(light_source_end_point - light_source_start_point)
+		vector_incr_direction = normalize_vector(end_point - start_point)
+		vector_norm = np.linalg.norm(end_point - start_point)
 		self.vector_incr = vector_incr_direction * (vector_norm / self.nb_frames)
 
-	def animate(self):
+	def animate_camera(self):
+		def modify_camera(new_point):
+			self.scene.modify_camera(Camera(new_point))
+		self.__animate(modify_camera)
+
+	def animate_light(self):
+		def modify_light_source(new_point):
+			self.scene.modify_light_source(Light(new_point))
+		self.__animate(modify_light_source)
+
+	def __animate(self, scene_modification_func):
 		excessive_computation_count = 0
+		excessive_computations = []
 
 		for i in range(0, self.nb_frames + 1):
 			before_comp_time = time.time()
 
-			point = self.light_source_start_point + self.vector_incr * i
-			self.scene.modify_light_source(Light(point))
+			point = self.start_point + self.vector_incr * i
+			scene_modification_func(point)
 			self.scene.show()
 
 			after_comp_time = time.time()
@@ -32,7 +46,26 @@ class Animation:
 
 			if sleep_time < 0:
 				excessive_computation_count += 1
+				excessive_computations.append(comp_time)
 				if excessive_computation_count >= 3:
-					print("WARNING: computation regularly takes more time than {} (1/FPS)".format(1 / self.FPS))
+					print("WARNING: computation regularly takes more time than {} (1/FPS) (takes an average of {:.3f}s)".format(1 / self.FPS, np.mean(excessive_computations)))
 
 			time.sleep(max(sleep_time, 0))
+
+	@staticmethod
+	def middle_to_up_camera_animation(scene):
+		start_point = np.array([7, 0, 3])
+		end_point = np.array([7, 0, 4])
+		return Animation(scene, start_point, end_point)
+
+	@staticmethod
+	def middle_to_right_light_animation(scene):
+		start_point = np.array([3, 0, 0])
+		end_point = np.array([0, 3, 4])
+		return Animation(scene, start_point, end_point)
+
+	@staticmethod
+	def down_to_up_light_animation(scene):
+		start_point = np.array([3, 0, - 2])
+		end_point = np.array([3, 0, 2])
+		return Animation(scene, start_point, end_point)

main.py
@@ -6,26 +6,19 @@ from scene.scene import Scene
 from scene.camera import Camera
 from scene.light import Light
 from animation import Animation
+from rotation import Rotation
 
 if __name__ == "__main__":
-	R = 1
-	r = 0.2
+	obj = Torus()
 
-	#obj = Torus(R, r)
-	obj = Sphere(R)
-	#obj = Line()
-
-	observer = np.array([7 * R, 0, 0])
-	light_source = Light(np.array([10 * R, 0, 0]))
+	observer = np.array([7, 0, 3])
+	light_source = Light(np.array([10, 0, 0]))
 	camera = Camera(observer)
 	scene = Scene(obj, light_source, observer, camera)
+	scene.show()
 
-	light_source_start_point = np.array([3 * R, 0, 0])
-	light_source_end_point = np.array([0, 3 * R, 4 * R])
-	light_animation = Animation(scene, light_source_start_point, light_source_end_point)
+	# light_animation = Animation.middle_to_up_camera_animation(scene)
+	# light_animation.animate_camera()
 
-	light_animation.animate()
-
-	#import cProfile
-	#cProfile.run('light_animation.animate()')
-	
+	rotation_animation = Rotation.full_around_y(scene)
+	rotation_animation.animate()

objects/line.py
@@ -1,13 +1,13 @@
 import numpy as np
-from utils import normalize_vector
+from utils import normalize_vector, rotate
 
 class Line:
 	def __init__(self, distance_to_origin=5, step=0.01):
 		self.distance_to_origin = distance_to_origin
 		self.step = step
 		self.position_along_axis = [position for position in np.arange(-self.distance_to_origin, self.distance_to_origin, self.step)]
-		self.points = [self.point_equation(position) for position in self.position_along_axis]
-		self.normals = [self.normal_equation(position) for position in self.position_along_axis]
+		self.points = np.transpose([self.point_equation(position) for position in self.position_along_axis])
+		self.normals = np.transpose([self.normal_equation(position) for position in self.position_along_axis])
 
 	def point_equation(self, step):
 		return np.array([
@@ -22,3 +22,6 @@ class Line:
 				0,
 				0
 			]))
+
+	def rotate(self, rotation_matrix_func, theta):
+		self.points, self.normals = rotate(rotation_matrix_func, theta, self.points, self.normals)