d5aaa59e63f0154dbdbcd3f0c6a9123341074f61
Luca Albinati <luca.albinati@gmail.com>
2021-10-27 08:25:03 +0200

    move around if-else statement to evaluate only what we need when we need it

--- scene/light.py
+++ scene/light.py
@@ -8,18 +8,18 @@ class Light:
 
 	def compute_brightness_on_point(self, observer, point_position, point_normal):
 		point_to_observer_vector = observer - point_position
-		point_to_light_vector = self.position - point_position
-
 		point_to_observer_factor = np.dot(point_to_observer_vector, point_normal)
-		point_to_light_factor = np.dot(point_to_light_vector, point_normal)
-
 		is_visible_to_the_observer = point_to_observer_factor > 0
-		is_casting_light_on_point = point_to_light_factor > 0
 
-		if is_visible_to_the_observer and is_casting_light_on_point:
-			distance_factor = 1 / np.sum(np.square(point_to_light_vector))
-			return self.intensity * distance_factor * point_to_observer_factor * point_to_light_factor + self.ambient_light
-		elif is_visible_to_the_observer:
-			return self.ambient_light
+		if is_visible_to_the_observer:
+			point_to_light_vector = self.position - point_position
+			point_to_light_factor = np.dot(point_to_light_vector, point_normal)
+			is_casting_light_on_point = point_to_light_factor > 0
+
+			if is_casting_light_on_point:
+				distance_factor = 1 / np.sum(np.square(point_to_light_vector))
+				return self.intensity * distance_factor * point_to_observer_factor * point_to_light_factor + self.ambient_light
+			else:
+				return self.ambient_light
 		else:
 			return 0
