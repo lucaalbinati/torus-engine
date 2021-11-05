7865cff094487377a1053c505e2c501fea70f3f8
Luca Albinati <luca.albinati@gmail.com>
2021-11-05 09:25:55 +0100

    better manage the bumping of versions

---
+++ bump_and_publish.py
@@ -0,0 +1,33 @@
+import sys
+import config
+import subprocess
+
+command_options = {
+    "-M":   "major upgrade",
+    "-m":   "minor upgrade",
+    "-p":   "patch upgrade"
+}
+
+if len(sys.argv) != 2:
+    message = "Must pass an argument to indicate which kind of version upgrade this is:\n"
+    for option, description in command_options.items():
+        message += "\t{}\t\t{}\n".format(option, description)
+    raise Exception(message)
+
+type_of_upgrade = sys.argv[1]
+if not type_of_upgrade in command_options.keys():
+    raise Exception("Unknown type of version upgrade: '{}'".format(type_of_upgrade))
+    
+# bump version
+subprocess.run(["bump", type_of_upgrade, "-r"])
+
+# build python project
+subprocess.run(["python", "-m", "build"])
+
+# publish using twine
+try:
+    subprocess.run(["twine", "upload", "dist/*", "-u", config.username, "-p", config.password])
+finally:
+    # remove build files
+    subprocess.run(["rm", "-r", "dist"])
+    subprocess.run(["rm", "-r", "src/torus_engine.egg-info"])
