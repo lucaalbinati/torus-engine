2e94365275fe1ae56766c63393b64a8110b61a70
Luca Albinati <luca.albinati@gmail.com>
2021-11-05 10:57:38 +0100

    fix urls

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setuptools.setup(
     author="Luca Albinati",
     author_email="luca.albinati@gmail.com",
     description="3D ASCII character rendering engine",
-    url="https://github.com/lucaalbinati/Torus",
+    url="https://github.com/lucaalbinati/torus-engine",
     project_urls={
-        "Bug Tracker": "https://github.com/lucaalbinati/Torus/issues",
+        "Bug Tracker": "https://github.com/lucaalbinati/torus-engine/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
