1e65c8df99ca7e0b3eb49d501f0158d53188c459
Luca Albinati <luca.albinati@gmail.com>
2021-12-04 10:54:04 +0100

    patch release v1.3.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = open(os.path.join(rootdir, 'README.md')).read()
 
 setuptools.setup(
     name="torus-engine",
-    version="1.3.1",
+    version="1.3.2",
     author="Luca Albinati",
     author_email="luca.albinati@gmail.com",
     description="3D ASCII character rendering engine",
