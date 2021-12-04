f762a755c9fb9223fddfdd3e491566c0846c7889
Luca Albinati <luca.albinati@gmail.com>
2021-12-04 11:04:27 +0100

    fix 'package_dir'

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "torusengine"},
+    package_dir={"": "."},
     packages=setuptools.find_packages(where="torusengine"),
     python_requires=">=3.7",
 )
